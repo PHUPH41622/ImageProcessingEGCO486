433082b8371a996db6430b8d0d090ef2db488bbb
PHUPH41622 <143483485+PHUPH41622@users.noreply.github.com>
2024-11-03 22:55:37 +0700

    Add utils.py and calculate total price

--- src/predict.py
+++ src/predict.py
@@ -1,16 +1,17 @@
 import os
 import cv2
 from ultralytics import YOLO
+from utils import map_result_to_price
 
 # Set paths
 IMAGES_DIR = 'validate_data/images'
-image_path = os.path.join(IMAGES_DIR, 'test6.jpg')
+image_path = os.path.join(IMAGES_DIR, 'test2.jpg')
 output_image_path = '{}_out.jpg'.format(image_path)
 
 # Load the image
 image = cv2.imread(image_path)
 
-# Load a model
+# Load a model and weights
 model_path = os.path.join('.', 'runs', 'detect', 'train', 'weights', 'best.pt')
 model = YOLO(model_path)  # load a custom model
 
@@ -18,16 +19,20 @@ threshold = 0.5
 
 # Predict on the image
 results = model(image)[0]
+result_list = []
 
 # Draw bounding boxes on the image
 for result in results.boxes.data.tolist():
     x1, y1, x2, y2, score, class_id = result
 
     if score > threshold:
-        cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+        cv2.rectangle(image, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 4)
         cv2.putText(image, results.names[int(class_id)].upper(), (int(x1), int(y1) - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+                    cv2.FONT_HERSHEY_SIMPLEX, 1.3, (255, 0, 0), 3, cv2.LINE_AA)
+    result_list.append(int(class_id))
 
-# Save the output image
+cv2.putText(image, "Total Price: {}".format(map_result_to_price(result_list)), (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 0, 0), 8, cv2.LINE_AA)
+
+# show output image
 cv2.imshow("output (press any key to quit)", image)
 cv2.waitKey(0) # Wait for a key press
--- src/video_predict.py
+++ src/video_predict.py
@@ -1,6 +1,6 @@
 import os
 import time
-
+from utils import map_result_to_price
 from ultralytics import YOLO
 import cv2
 import numpy as np
@@ -24,7 +24,7 @@ threshold = 0.5
 
 while ret:
     results = model(frame)[0]
-
+    result_list = []
     for result in results.boxes.data.tolist():
         x1, y1, x2, y2, score, class_id = result
 
@@ -32,8 +32,10 @@ while ret:
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
             cv2.putText(frame, results.names[int(class_id)].upper(), (int(x1), int(y1 - 10)),
                         cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+        result_list.append(int(class_id))
 
     # Display the frame with predictions
+    cv2.putText(frame, "Total Price: {}".format(map_result_to_price(result_list)), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
     cv2.imshow('Predictions', frame)
     
     # Wait for a short time to display frames; press 'q' to exit
